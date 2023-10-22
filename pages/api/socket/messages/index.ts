504484edf758f2832b1dfa69155ebff10cf924b0
NANAIPRM <porramatc@baania.com>
2023-10-22 11:36:31 +0700

    chatitem

--- pages/api/socket/messages/index.ts
+++ pages/api/socket/messages/index.ts
@@ -16,19 +16,19 @@ export default async function handler(
     const profile = await currentProfilePages(req);
     const { content, fileUrl } = req.body;
     const { serverId, channelId } = req.query;
-
+    
     if (!profile) {
       return res.status(401).json({ error: "Unauthorized" });
     }    
-
+  
     if (!serverId) {
       return res.status(400).json({ error: "Server ID missing" });
     }
-
+      
     if (!channelId) {
       return res.status(400).json({ error: "Channel ID missing" });
     }
-
+          
     if (!content) {
       return res.status(400).json({ error: "Content missing" });
     }
